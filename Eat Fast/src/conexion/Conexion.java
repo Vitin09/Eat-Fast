c372ab233bf88840b4297ac5fed60f8f317d7011
Victin09 <39098525+Victin09@users.noreply.github.com>
2018-05-08 19:44:47 +0200

    Primera Subida

---
+++ Eat Fast/src/conexion/Conexion.java
@@ -0,0 +1,38 @@
+package conexion;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import proyectoDAO.Cliente;
+import utils.HibernateUtils;
+
+public class Conexion {
+
+	private static Session session;
+	
+	public static void main(String[] args) {
+
+		//Abrir sesion con Hibernate
+		session = HibernateUtils.getSessionFactory().openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		//Hacer commit
+		boolean commit = true;
+		
+		crearClinte("Viti", "87654321B", "Calle de Raquel", 987465123);
+		
+		//Hacer commit BBDD
+		if (commit) {			
+			transaction.commit();
+		}
+		//Cerrar la sesion
+		session.close();
+		
+		System.exit(0);
+	}
+
+	public static void crearClinte(String nombre, String dni, String direccion, int telefono) {
+		Cliente c = new Cliente(nombre, dni, direccion, telefono, null);
+		session.save(c);
+	}
+}
